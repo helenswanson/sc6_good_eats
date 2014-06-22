807e193d4e7f8f586836a5e86c7872d48f8ebf63
helenswanson <hhyswanson@gmail.com>
2014-06-22 17:20:31 -0400

    Improved models.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,12 @@
-class Restaurant < ActiveRecord::Base
-  belongs_to :restaurants
+class Review < ActiveRecord::Base
+  belongs_to :restaurant
 
-  validates :rating, presence: true, :inclusion { :in 1..5 }
+  # validates_numericality_of :rating, presence: true, :only_integer => true,
+  #   :message => "can only be whole number."
+  # validates_inclusion_of :rating, :in => 1..5,
+  #   :message => "can only be between 1 and 5."
+
+  validates :rating, presence: true
   validates :body, presence: true
   validates :created_at, presence: true
   validates :restaurant_id, presence: true
