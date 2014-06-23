1349d51122b1fd6e233ee4e0dc0f14e59934d66d
helenswanson <hhyswanson@gmail.com>
2014-06-22 21:14:24 -0400

    Added state to restaurant migration.

--- db/migrate/20140621221713_create_restaurants.rb
+++ db/migrate/20140621221713_create_restaurants.rb
@@ -4,6 +4,7 @@ class CreateRestaurants < ActiveRecord::Migration
       t.string    :name,        null: false
       t.string    :address,     null: false
       t.string    :city,        null: false
+      t.string    :state,       null: false
       t.integer   :zip_code,    null: false
       t.text      :description
       t.string    :category
