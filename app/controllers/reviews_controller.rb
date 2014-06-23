b4bb4f4a2abaad48eb0108cdafc023ffdd6f7f6d
helenswanson <hhyswanson@gmail.com>
2014-06-22 22:08:48 -0400

    Updated create and review_params in controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,14 +11,13 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
-    @review.restaurant_id = @restaurant.id
 
     if @review.save
       flash[:notice] = "Success!"
     else
       flash.now[:notice] = "Your review couldn't be submitted."
     end
-    redirect_to "/restaurants/#{@restaurant.id}"
+    redirect_to @restaurant
   end
 
   private
